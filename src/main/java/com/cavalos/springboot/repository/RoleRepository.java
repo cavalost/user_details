31ee0a8e798358716f564b2711093b2db00847b5
cavalost <claudina.avalos@opendeusto.es>
2020-12-25 23:59:12 +0000

    feat: add role

--- src/main/java/com/cavalos/springboot/repository/RoleRepository.java
+++ src/main/java/com/cavalos/springboot/repository/RoleRepository.java
@@ -1,4 +1,12 @@
 package com.cavalos.springboot.repository;
 
-public interface RoleRepository {
+import org.springframework.data.mongodb.repository.MongoRepository;
+import com.cavalos.springboot.model.Role;
+import com.cavalos.springboot.model.ERole;
+import java.util.Optional;
+
+public interface RoleRepository extends MongoRepository<Role, String> {
+
+    Optional<Role> findByName(ERole, name);
+
 }
--- src/main/java/com/cavalos/springboot/repository/UserRepository.java
+++ src/main/java/com/cavalos/springboot/repository/UserRepository.java
@@ -3,5 +3,12 @@ package com.cavalos.springboot.repository;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import com.cavalos.springboot.model.User;
 
+import java.util.Optional;
+
 public interface UserRepository extends MongoRepository<User, String>{
+
+    Optional<User> findByUsername(String username);
+    Boolean existByUsername(String username);
+    Boolean existByEmail(String email);
+
 }
